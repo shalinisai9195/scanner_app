522a4a623e9ebca6a2393e694d4916b314f9edbe
Thenmozhi L <110511957+shalinisai9195@users.noreply.github.com>
2024-07-03 09:38:06 +0530

    change_revote
    edges changed code not work so we replace the same

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -80,88 +80,15 @@ export class ScannerComponent {
       }
     }
 
-    // const rect = cv.boundingRect(maxContour);
-    // const cropped = src.roi(rect);
-    // cv.imshow(context.canvas, cropped);
+    const rect = cv.boundingRect(maxContour);
+    const cropped = src.roi(rect);
+    cv.imshow(context.canvas, cropped);
 
-    // src.delete(); gray.delete(); contours.delete(); hierarchy.delete();
-
-    // this.processedImage = context.canvas.toDataURL('image/png');
-
-    const peri = cv.arcLength(maxContour, true);
-    const approx = new cv.Mat();
-    cv.approxPolyDP(maxContour, approx, 0.02 * peri, true);
-
-    if (approx.rows === 4) {
-      const points = [];
-      for (let i = 0; i < 4; i++) {
-        points.push({
-          x: approx.data32S[i * 2],
-          y: approx.data32S[i * 2 + 1]
-        });
-      }
-
-      // Sort points to get top-left, top-right, bottom-right, and bottom-left
-      points.sort((a, b) => a.y - b.y);
-      let topLeft, topRight, bottomLeft, bottomRight;
-      if (points[0].x < points[1].x) {
-        topLeft = points[0];
-        topRight = points[1];
-      } else {
-        topLeft = points[1];
-        topRight = points[0];
-      }
-      if (points[2].x < points[3].x) {
-        bottomLeft = points[2];
-        bottomRight = points[3];
-      } else {
-        bottomLeft = points[3];
-        bottomRight = points[2];
-      }
-
-      // Draw the detected edges on the canvas
-      context.beginPath();
-      context.moveTo(topLeft.x, topLeft.y);
-      context.lineTo(topRight.x, topRight.y);
-      context.lineTo(bottomRight.x, bottomRight.y);
-      context.lineTo(bottomLeft.x, bottomLeft.y);
-      context.closePath();
-      context.lineWidth = 3;
-      context.strokeStyle = 'red';
-      context.stroke();
-
-      // Apply perspective transformation
-      const dst = new cv.Mat();
-      const dsize = new cv.Size(context.canvas.width, context.canvas.height);
-      const srcTri = cv.matFromArray(4, 1, cv.CV_32FC2, [
-        topLeft.x, topLeft.y,
-        topRight.x, topRight.y,
-        bottomRight.x, bottomRight.y,
-        bottomLeft.x, bottomLeft.y
-      ]);
-      const dstTri = cv.matFromArray(4, 1, cv.CV_32FC2, [
-        0, 0,
-        dsize.width, 0,
-        dsize.width, dsize.height,
-        0, dsize.height
-      ]);
-      const M = cv.getPerspectiveTransform(srcTri, dstTri);
-      cv.warpPerspective(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-
-      cv.imshow(context.canvas, dst);
-
-      srcTri.delete(); dstTri.delete(); dst.delete(); M.delete();
-    } else {
-      // If no quadrilateral found, fallback to bounding box
-      const rect = cv.boundingRect(maxContour);
-      const cropped = src.roi(rect);
-      cv.imshow(context.canvas, cropped);
-      cropped.delete();
-    }
-
-    src.delete(); gray.delete(); contours.delete(); hierarchy.delete(); maxContour.delete(); approx.delete();
+    src.delete(); gray.delete(); contours.delete(); hierarchy.delete();
 
     this.processedImage = context.canvas.toDataURL('image/png');
+
+    
   }
 
   downloadImage() {
